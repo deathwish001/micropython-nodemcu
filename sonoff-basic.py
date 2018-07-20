d65ed3099523004545579e09675345e8ef970e39
deathwish001 <de@thwish.com>
2018-07-21 00:14:07 +1200

    Create sonoff-basic.py

---
+++ sonoff-basic.py
@@ -0,0 +1,47 @@
+# MIT License
+# 
+# Copyright (c) 2018 deathwish.xyz
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+Sonoff Basic R2
+V1.0 
+ 
+~Sonoff Pins
+GPIO0 = Pushbutton
+GPIO12 = Relay
+GPIO13 = Green LED
+GPIO14 = Spare on PCB
+
+~Stuff to do:
+Setup pins
+function to control pushbutton with callback
+function to control relay output
+function to control led output (maybe some flashers?)
+function for spare input, basic callback
+
+'''
+
+
+
+
+
+
+
